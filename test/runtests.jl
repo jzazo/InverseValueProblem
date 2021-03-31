a28386441cfcb67b044c95217d9ee4aee2cd478b
jzazo <v-javierzazo@microsoft.com>
2021-03-31 13:04:16 +0000

    Add tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -56,3 +56,32 @@ function test_min_fpidelta()
 end
 
 test_min_fpidelta()
+
+
+"""Minimizes f(ϵ, c; ck, cα, λ, xi) = ϵ'1 + 1/(2λ) ‖c - ck‖^2 + α/2 ‖c - cα‖^2,
+s.t., c'xi - zi ≤ ϵi, ϵi ≥ 0, for all i, c ∈ C.
+
+"""
+function test_fcepsilon()
+    n = 20  # dimensions
+    k = 10  # number of measurements
+    xi = rand(n, k)  # feature vector
+    zi = 2 * rand(k)  # measurements
+    ck = rand(n)
+    cα = rand(n)
+    λ = 1.0
+    α = 1.0
+
+    model = Model(OSQP.Optimizer)
+    @variable(model, ϵi[1:k] >= 0)
+    @variable(model, c[1:n])
+    @objective(model, Min, sum(ϵi) + 1.0 / 2.0 / λ * sum((c - ck).^2) + α / 2.0 * sum((c - cα).^2))
+    @constraint(model, con[i=1:k], xi[:, i]'c - zi[i] - ϵi[i] <= 0.0)
+    optimize!(model)
+
+    c_sol1 = value.(c)
+    c_sol2 = ClassificatinoInerseValueProblem.min_fcepsilon(xi, zi, ck, cα, λ, α)
+    @test c_sol1 ≈ c_sol2 rtol = 1e-3 * n
+end
+
+test_fcepsilon()
